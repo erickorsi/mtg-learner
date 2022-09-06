0e57e76fa872ebfed05f00f037e7782b7d720405
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-05 21:15:48 -0300

    Training main

---
+++ model_training/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 05 20:27:00 2022
+
+@author: https://github.com/erickorsi
+
+Main script for training text classification models using an ensemble of
+LSTM and SVC.
+"""
