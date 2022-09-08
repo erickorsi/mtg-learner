8671034cf3403dc3d8fa6e73190b249f4ac37c14
Erick Hotta Orsi <33377887+erickorsi@users.noreply.github.com>
2022-09-07 22:49:37 -0300

    Start of class structure

--- model_training/main.py
+++ model_training/main.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 05 20:27:00 2022
+Created on Mon Sep 05 20:27 2022
 
 @author: https://github.com/erickorsi
 
 Main script for training text classification models using an ensemble of
 LSTM and SVC.
 """
+
+class BrocaClassifier():
+    '''
+    '''
+    def fit(x, y, language='', ignore_punctuation=False, cross_validation=0):
+        '''
+        '''
+        # Natural Language Processing of the texts
+        x = nlp(x, language)
+
+        # Cross validate
+        cv
+
+        # First step based on vocabulary (word2vec NN)
+        vocab =
+
+        # Second step based on grammatical structure (LSTM)
+        grammar = LSTM(x, y)
+
+        # Return object with models attributes
+        return self
